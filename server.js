01b582e377d4433fe5ffa40b3ac6e0d7a441cefd
Leon Noel <leon.a.noel+github@gmail.com>
2022-06-02 18:11:32 -0700

    added heroku port

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get('/api/:name',(request,response)=>{
     
 })
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is now running on port ${PORT}! Betta Go Catch It!`)
 })
